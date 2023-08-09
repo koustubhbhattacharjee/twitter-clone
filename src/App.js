405adeba26ef395a20ae88321ce09d6f973d1d38
koustubhbhattacharjee <kbohuastt@gmail.com>
2023-08-09 16:32:41 +0000

    FC

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Let's build a twitter clone </h1>
+      {/* Sidebar */}
+
+      {/* feed */}
+      
+      {/* widgets */}
+      
+    </div>
+  );
+}
+
+export default App;
